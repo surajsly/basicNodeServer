24ca291c011ef183204bf0f6af241312f90d2a38
Suraj Singh <50519406+surajsly@users.noreply.github.com>
2020-06-26 10:04:13 +0530

    sending response

--- app.js
+++ app.js
@@ -3,8 +3,14 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log(req);
+  console.log(req.url, req.method, req.headers);
   //process.exit();
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>suraj</title></head>");
+  res.write("<body><h1>hellow from my nodeJs server</h1></body>");
+  res.write("</html>");
+  res.end();
 });
 
 server.listen(3000);
