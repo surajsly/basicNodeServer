110daf806d42b9633868505d4d515dcfb0e11b9c
Suraj Singh <50519406+surajsly@users.noreply.github.com>
2020-06-26 10:42:28 +0530

    storing form data into a file

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 // setting up my first node server.
 
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   //console.log(req.url, req.method, req.headers);
   //process.exit();
   const url = req.url;
+  const method = req.method;
+
   if (url === "/") {
     res.setHeader("Content-Type", "text/html");
     res.write("<html>");
@@ -16,6 +19,26 @@ const server = http.createServer((req, res) => {
     res.write("</html>");
     return res.end();
   }
+
+  if (url === "/message" && method == "POST") {
+    const body = [];
+
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      console.log(parsedBody);
+      const message = parsedBody.split("=")[1];
+      fs.writeFileSync("message.txt", message);
+    });
+
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    return res.end();
+  }
+
   res.setHeader("Content-Type", "text/html");
   res.write("<html>");
   res.write("<head><title>suraj</title></head>");
