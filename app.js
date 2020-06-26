2944e55c1a65740246bad9733913a7e62b5c530a
Suraj Singh <50519406+surajsly@users.noreply.github.com>
2020-06-26 10:14:15 +0530

    custom html response

--- app.js
+++ app.js
@@ -3,8 +3,19 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.method, req.headers);
+  //console.log(req.url, req.method, req.headers);
   //process.exit();
+  const url = req.url;
+  if (url === "/") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>suraj</title></head>");
+    res.write(
+      "<body><form action='/message' method='POST'><input name='message'type='text'><button type='submit'>Send</button></form></body>"
+    );
+    res.write("</html>");
+    return res.end();
+  }
   res.setHeader("Content-Type", "text/html");
   res.write("<html>");
   res.write("<head><title>suraj</title></head>");
